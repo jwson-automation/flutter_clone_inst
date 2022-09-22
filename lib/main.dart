277489965aef915792741a916e5348182c1fb7eb
jwson-automation <sonjungwoo9@gmail.com>
2022-09-22 21:01:08 +0900

    connect with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_inst/firebase_options.dart';
 import 'package:flutter_clone_inst/src/binding/init_bindings.dart';
 import 'package:get/get.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
